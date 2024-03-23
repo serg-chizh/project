0e101074ddf9e432389890e684e0e86a1f32b59a
serg-chizh <164508106+serg-chizh@users.noreply.github.com>
2024-03-23 13:07:14 +0300

    Unit1ИП.pas

--- Unit1.pas
+++ Unit1.pas
@@ -3,33 +3,32 @@ unit Unit1;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, TeeProcs, TeEngine, Chart, ExtCtrls, StdCtrls, ComCtrls, Series;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, VclTee.TeeGDIPlus, Vcl.ComCtrls,
+  Vcl.StdCtrls, VCLTee.TeEngine, VCLTee.Series, VCLTee.TeeProcs, VCLTee.Chart,
+  Vcl.ExtCtrls, System.ImageList, Vcl.ImgList, Vcl.Imaging.GIFImg;
 
 type
   TForm1 = class(TForm)
     Panel1: TPanel;
-    Splitter1: TSplitter;
     Panel2: TPanel;
     Panel3: TPanel;
-    Panel4: TPanel;
-    Chart1: TChart;
-    Button1: TButton;
-    TrackBar1: TTrackBar;
-    TrackBar2: TTrackBar;
-    TrackBar3: TTrackBar;
     RadioButton1: TRadioButton;
     RadioButton2: TRadioButton;
     RadioButton3: TRadioButton;
-    Series1: TFastLineSeries;
-    Series2: TFastLineSeries;
-    Series3: TFastLineSeries;
+    RadioButton4: TRadioButton;
+    TrackBar1: TTrackBar;
+    TrackBar2: TTrackBar;
+    TrackBar3: TTrackBar;
+    Chart1: TChart;
+    Series1: TLineSeries;
+    Series2: TLineSeries;
+    Series3: TLineSeries;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Timer1: TTimer;
-    RadioButton4: TRadioButton;
-    Label4: TLabel;
+    Button1: TButton;
     function Moschost(z:byte; p:real; s:integer; v:real):real;
     procedure Timer1Timer(Sender: TObject);
     procedure Button1Click(Sender: TObject);
@@ -41,6 +40,7 @@ type
     procedure TrackBar1Change(Sender: TObject);
     procedure TrackBar2Change(Sender: TObject);
     procedure TrackBar3Change(Sender: TObject);
+    procedure Panel1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -54,13 +54,19 @@ implementation
 
 {$R *.dfm}
 
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if timer1.Enabled=false then timer1.Enabled:=true
+                          else timer1.Enabled:=false;
+end;
+
 function TForm1.Moschost(z:byte; p:real; s:integer; v:real): real;
   var t:real;
 begin
 
 case z of
-{ğàáîòà Ñàâîíèóñà 1-òèï ãåíåğàòîğà
- ñ 1 ìåòğà íà÷èíàåò ğàáîòàòü äî 4 ïîòîì ïîòîëîê äî øåñòè ïîòîì òîğìîçíîé ìåõàíèçì îñòàííàâëèâàåòñÿ äëÿ ïåğäîòâğàùåíèÿ ğàçğóøåíèÿ}
+{Ñ€Ğ°Ğ±Ğ¾Ñ‚Ğ° Ğ¡Ğ°Ğ²Ğ¾Ğ½Ğ¸ÑƒÑĞ° 1-Ñ‚Ğ¸Ğ¿ Ğ³ĞµĞ½ĞµÑ€Ğ°Ñ‚Ğ¾Ñ€Ğ°
+ Ñ 1 Ğ¼ĞµÑ‚Ñ€Ğ° Ğ½Ğ°Ñ‡Ğ¸Ğ½Ğ°ĞµÑ‚ Ñ€Ğ°Ğ±Ğ¾Ñ‚Ğ°Ñ‚ÑŒ Ğ´Ğ¾ 4 Ğ¿Ğ¾Ñ‚Ğ¾Ğ¼ Ğ¿Ğ¾Ñ‚Ğ¾Ğ»Ğ¾Ğº Ğ´Ğ¾ ÑˆĞµÑÑ‚Ğ¸ Ğ¿Ğ¾Ñ‚Ğ¾Ğ¼ Ñ‚Ğ¾Ñ€Ğ¼Ğ¾Ğ·Ğ½Ğ¾Ğ¹ Ğ¼ĞµÑ…Ğ°Ğ½Ğ¸Ğ·Ğ¼ Ğ¾ÑÑ‚Ğ°Ğ½Ğ½Ğ°Ğ²Ğ»Ğ¸Ğ²Ğ°ĞµÑ‚ÑÑ Ğ´Ğ»Ñ Ğ¿ĞµÑ€Ğ´Ğ¾Ñ‚Ğ²Ñ€Ğ°Ñ‰ĞµĞ½Ğ¸Ñ Ñ€Ğ°Ğ·Ñ€ÑƒÑˆĞµĞ½Ğ¸Ñ}
   1: begin
        t:=0.15;
        if (v>=1)and (v<=4) then result:= 0.5*t*p*s*(v*v*v)
@@ -76,8 +82,8 @@ case z of
                                                      end;
 
      end;
-{ğàáîòà Äàğüå
- ñ 5 ìåòğîâ ğàáîòàåò äî 10 ìåòğîâ ïîñëå òîğìîçíîé ìåõàíèçì îñòàííàâëèâàåòñÿ äëÿ ïåğäîòâğàùåíèÿ ğàçğóøåíèÿ}
+{Ñ€Ğ°Ğ±Ğ¾Ñ‚Ğ° Ğ”Ğ°Ñ€ÑŒĞµ
+ Ñ 5 Ğ¼ĞµÑ‚Ñ€Ğ¾Ğ² Ñ€Ğ°Ğ±Ğ¾Ñ‚Ğ°ĞµÑ‚ Ğ´Ğ¾ 10 Ğ¼ĞµÑ‚Ñ€Ğ¾Ğ² Ğ¿Ğ¾ÑĞ»Ğµ Ñ‚Ğ¾Ñ€Ğ¼Ğ¾Ğ·Ğ½Ğ¾Ğ¹ Ğ¼ĞµÑ…Ğ°Ğ½Ğ¸Ğ·Ğ¼ Ğ¾ÑÑ‚Ğ°Ğ½Ğ½Ğ°Ğ²Ğ»Ğ¸Ğ²Ğ°ĞµÑ‚ÑÑ Ğ´Ğ»Ñ Ğ¿ĞµÑ€Ğ´Ğ¾Ñ‚Ğ²Ñ€Ğ°Ñ‰ĞµĞ½Ğ¸Ñ Ñ€Ğ°Ğ·Ñ€ÑƒÑˆĞµĞ½Ğ¸Ñ}
   2: begin
        t:=0.30;
        if (v>=5)and (v<=10) then result:= 0.5*t*p*s*(v*v*v)
@@ -89,8 +95,8 @@ case z of
 
      end;
 
-{ğàáîòà ãîğèçîíòàëüíûé
- ñ 5 ìåòğîâ ğàáîòàåò äî 15 ğàñòåò íîğìàëüíî ïîòîì òîğìîçíîé ìåõàíèçì îñòàííàâëèâàåòñÿ äëÿ ïåğäîòâğàùåíèÿ ğàçğóøåíèÿ}
+{Ñ€Ğ°Ğ±Ğ¾Ñ‚Ğ° Ğ³Ğ¾Ñ€Ğ¸Ğ·Ğ¾Ğ½Ñ‚Ğ°Ğ»ÑŒĞ½Ñ‹Ğ¹
+ Ñ 5 Ğ¼ĞµÑ‚Ñ€Ğ¾Ğ² Ñ€Ğ°Ğ±Ğ¾Ñ‚Ğ°ĞµÑ‚ Ğ´Ğ¾ 15 Ñ€Ğ°ÑÑ‚ĞµÑ‚ Ğ½Ğ¾Ñ€Ğ¼Ğ°Ğ»ÑŒĞ½Ğ¾ Ğ¿Ğ¾Ñ‚Ğ¾Ğ¼ Ñ‚Ğ¾Ñ€Ğ¼Ğ¾Ğ·Ğ½Ğ¾Ğ¹ Ğ¼ĞµÑ…Ğ°Ğ½Ğ¸Ğ·Ğ¼ Ğ¾ÑÑ‚Ğ°Ğ½Ğ½Ğ°Ğ²Ğ»Ğ¸Ğ²Ğ°ĞµÑ‚ÑÑ Ğ´Ğ»Ñ Ğ¿ĞµÑ€Ğ´Ğ¾Ñ‚Ğ²Ñ€Ğ°Ñ‰ĞµĞ½Ğ¸Ñ Ñ€Ğ°Ğ·Ñ€ÑƒÑˆĞµĞ½Ğ¸Ñ}
   3: begin
        t:=0.45;
        if (v>=5)and (v<=15) then result:= 0.5*t*p*s*(v*v*v)
@@ -105,37 +111,9 @@ end {case};
 end {func};
 
 
-procedure TForm1.Timer1Timer(Sender: TObject);
-
+procedure TForm1.Panel1Click(Sender: TObject);
 begin
-if radiobutton1.Checked then chart1.Series[0].AddXY(TrackBar3.Position, Moschost(1, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
-if radiobutton2.Checked then chart1.Series[1].AddXY(TrackBar3.Position, Moschost(2, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
-if radiobutton3.Checked then chart1.Series[2].AddXY(TrackBar3.Position, Moschost(3, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
-
-if radiobutton4.Checked then
-  begin
-    chart1.Series[0].AddXY(TrackBar3.Position, Moschost(1, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
-    chart1.Series[1].AddXY(TrackBar3.Position, Moschost(2, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
-    chart1.Series[2].AddXY(TrackBar3.Position, Moschost(3, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
-  end;
-
- TrackBar3.Position:=TrackBar3.Position+1;
-end;
-
-procedure TForm1.Button1Click(Sender: TObject);
-begin
-  if timer1.Enabled=false then timer1.Enabled:=true
-                          else timer1.Enabled:=false;
-end;
-
-procedure TForm1.UpdGrafik;
-var i:byte;
-begin
-  for i:=0 to 2 do
-    chart1.Series[i].Clear;
-  TrackBar3.Position:=0;
-  if radiobutton4.Checked then chart1.Legend.Visible:=true
-                          else chart1.Legend.Visible:=false;
+//(Image1.Picture.Graphic as TGIFImage).Animate := True;
 end;
 
 procedure TForm1.RadioButton1Click(Sender: TObject);
@@ -158,21 +136,47 @@ begin
 UpdGrafik;
 end;
 
+procedure TForm1.UpdGrafik;
+var i:byte;
+begin
+  for i:=0 to 2 do
+    chart1.Series[i].Clear;
+  TrackBar3.Position:=0;
+  if radiobutton4.Checked then chart1.Legend.Visible:=true
+                          else chart1.Legend.Visible:=false;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+if radiobutton1.Checked then chart1.Series[0].AddXY(TrackBar3.Position, Moschost(1, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
+if radiobutton2.Checked then chart1.Series[1].AddXY(TrackBar3.Position, Moschost(2, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
+if radiobutton3.Checked then chart1.Series[2].AddXY(TrackBar3.Position, Moschost(3, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
+
+if radiobutton4.Checked then
+  begin
+    chart1.Series[0].AddXY(TrackBar3.Position, Moschost(1, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
+    chart1.Series[1].AddXY(TrackBar3.Position, Moschost(2, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
+    chart1.Series[2].AddXY(TrackBar3.Position, Moschost(3, TrackBar2.Position/10, TrackBar1.Position, TrackBar3.Position));
+  end;
+
+ TrackBar3.Position:=TrackBar3.Position+1;
+end;
+
 procedure TForm1.TrackBar1Change(Sender: TObject);
 begin
-label1.Caption:='Ïëîùàäü: '+inttostr(TrackBar1.Position)+' êâ.ì';
+label1.Caption:='ĞŸĞ»Ğ¾Ñ‰Ğ°Ğ´ÑŒ: '+inttostr(TrackBar1.Position)+' ĞºĞ².Ğ¼';
 end;
 
 procedure TForm1.TrackBar2Change(Sender: TObject);
 var ff:TFormatSettings;
 begin
 ff.CurrencyDecimals:=1;
-label2.Caption:='Ïëîòíîñòü âîçäóõà: '+floattostr(TrackBar2.Position/10)+' êóá.ì';
+label2.Caption:='ĞŸĞ»Ğ¾Ñ‚Ğ½Ğ¾ÑÑ‚ÑŒ Ğ²Ğ¾Ğ·Ğ´ÑƒÑ…Ğ°: '+floattostr(TrackBar2.Position/10)+' ĞºĞ³/ĞºÑƒĞ±.Ğ¼';
 end;
 
 procedure TForm1.TrackBar3Change(Sender: TObject);
 begin
-label3.Caption:='Ñêîğîñòü âåòğà: '+inttostr(TrackBar3.Position)+' ì/ñ';
+label3.Caption:='Ğ¡ĞºĞ¾Ñ€Ğ¾ÑÑ‚ÑŒ Ğ²ĞµÑ‚Ñ€Ğ°: '+inttostr(TrackBar3.Position)+' Ğ¼/Ñ';
 end;
 
 end.
